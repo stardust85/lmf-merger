376d4bad4a743463116c1d3d05180c1b824d8f0f
stardust1985@gmail.com <stardust1985@gmail.com@db4eac9c-27e5-1f69-70b9-b1cd029a92dc>
2011-03-05 20:00:54 +0000

    reordered

--- test-data/runtests.sh
+++ test-data/runtests.sh
@@ -8,12 +8,17 @@ file2=*2.xml
 
 for dir in test*
 do
-  cd $dir
-  if [ -f runtest.sh ]; then
-    ./runtest.sh
-  else
-    ../../src/lmf_merger.py $file1 $file2 merged.xml
-    diff -u merged.xml merged_correct.xml
-  fi
-  cd ..
+    cd $dir
+    if [ -f runtest.sh ]; then
+        ./runtest.sh
+    else
+        if [-f  mergerd.xml]; then 
+            rm merged.xml
+        fi
+        ../../src/lmf_merger.py $file1 $file2 merged.xml
+        diff -u merged.xml merged_correct.xml
+    fi
+    cd ..
 done
+
+
